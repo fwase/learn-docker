722a8f6c137859e6fabbf479a2a16bab2172894e
Fylype Wase <fylypexp@gmail.com>
2022-03-15 23:58:26 -0400

    rename file to main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Funcionando perfeitamente"}
+
+
+@app.get("/users")
+def get_users():
+    return [{"id": 0, "name": "fake_user"}]
